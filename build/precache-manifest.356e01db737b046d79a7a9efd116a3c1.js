b535ab4d58caea48b2f6e6906c7a94aadc670721
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2020-09-28 16:42:27 +0500

    build

---
+++ build/precache-manifest.356e01db737b046d79a7a9efd116a3c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3695f7a854b52c40c9e28f64551b43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd695f173d86a4f1d4e8",
+    "url": "/static/css/main.5abcebee.chunk.css"
+  },
+  {
+    "revision": "bf9c74f590833764e85b",
+    "url": "/static/js/2.6f321f9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f321f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd695f173d86a4f1d4e8",
+    "url": "/static/js/main.4a4a6ee9.chunk.js"
+  },
+  {
+    "revision": "9b1a3916d80a806f5cca",
+    "url": "/static/js/runtime-main.3964f847.js"
+  }
+]);
